5cf58bc54f69ac57b6d8bca72acac86d0085c41d
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-04-20 17:43:44 +0800

    chore: new image host

--- plugins/markdown-image.ts
+++ plugins/markdown-image.ts
@@ -1,15 +1,7 @@
 import type { RemarkPlugin } from '@astrojs/markdown-remark'
 import { visit } from 'unist-util-visit'
 
-// 备份一份至 sm.ms，用于在 jsDelivr 无法访问时使用
-// eslint-disable-next-line unused-imports/no-unused-vars
-const map: Record<string, string> = {
-    '/images/index-cover.jpg': 'https://s2.loli.net/2023/04/15/RlrGDhwZJ4t5zcW.jpg',
-    '/images/hello-world-cover.jpg': 'https://s2.loli.net/2023/04/15/cYWtoE4LQ7MZAwT.jpg',
-    '/images/naoelrie-wechat-mp.jpg': 'https://s2.loli.net/2023/04/15/ieGpTmfbMIcEq6h.jpg',
-}
-
-const jsDelivrBaseUrl = 'https://cdn.jsdelivr.net/npm/@jiakun-zhao/static/dist/personal-website'
+const jsDelivrBaseUrl = 'https://static.zhaojiakun.com/personal-website'
 
 export default function (): RemarkPlugin {
     return function () {
@@ -25,14 +17,6 @@ export default function (): RemarkPlugin {
                 props.onerror = 'src!=dataset.src&&(src=dataset.src)'
                 props.loading = 'lazy'
                 node.url = jsDelivrBaseUrl + node.url
-
-                // if (Object.hasOwn(map, node.url)) {
-                //     props['data-src'] = node.url
-                //     props.onerror = 'src!=dataset.src&&(src=dataset.src)'
-                //     node.url = map[node.url]
-                // } else {
-                //     props.loading = 'lazy'
-                // }
             })
         }
     }
