9e2f043a461f3bd4bf38cf6366acf1d79370c5b5
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-04-26 03:58:16 +0800

    chore: bug

--- astro.config.ts
+++ astro.config.ts
@@ -1,17 +1,17 @@
+import { fileURLToPath } from 'node:url'
+import { readFile, writeFile } from 'node:fs/promises'
+import type { AstroConfig, AstroIntegration } from 'astro'
+import type { RemarkPlugin } from '@astrojs/markdown-remark'
 import { defineConfig } from 'astro/config'
-import type { PluginOption } from 'vite'
-import cssnano from 'cssnano'
-import autoprefixer from 'autoprefixer'
 import { rollup } from 'rollup'
+import { visit } from 'unist-util-visit'
+import { minify } from 'html-minifier'
 import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
+import glob from 'fast-glob'
 import { CLIENT_SCRIPTS_NAME } from './src/config'
 
 export default defineConfig({
-    markdown: {
-        syntaxHighlight: false,
-        remarkPlugins: [],
-    },
     site: 'https://zhaojiakun.com',
     build: {
         assets: 'assets',
@@ -19,22 +19,20 @@ export default defineConfig({
     server: {
         host: true,
     },
+    integrations: [htmlMinify()],
+    markdown: {
+        syntaxHighlight: false,
+        remarkPlugins: [remarkLinkTarget()],
+    },
     vite: {
         optimizeDeps: { exclude: ['fsevents'] },
         build: { assetsInlineLimit: 0 },
-        css: {
-            postcss: {
-                plugins: [autoprefixer(), cssnano()],
-            },
-        },
-        plugins: [
-            injectClientScript(),
-        ],
+        plugins: [injectClientScript()],
     },
 })
 
-function injectClientScript(): PluginOption {
-    return {
+function injectClientScript(): AstroConfig['vite']['plugins'] {
+    return [{
         name: 'inject-client-scripts',
         resolveId(source) {
             return source === CLIENT_SCRIPTS_NAME ? `\0${CLIENT_SCRIPTS_NAME}` : undefined
@@ -48,6 +46,42 @@ function injectClientScript(): PluginOption {
                 const { output } = await bundle.generate({ format: 'iife' })
                 return output[0].code
             }
+        },
+    }]
+}
+
+function remarkLinkTarget(): RemarkPlugin {
+    return function () {
+        return function (tree) {
+            visit(tree, 'link', (node) => {
+                if (node.url.startsWith('http')) {
+                    const data = node.data || (node.data = {})
+                    const props: any = data.hProperties || (data.hProperties = {})
+                    props.target = '_blank'
+                }
+            })
+        }
+    }
+}
+
+function htmlMinify(): AstroIntegration {
+    return {
+        name: 'astro-html-minifier',
+        hooks: {
+            'astro:build:done': async ({ dir }) => {
+                const path = fileURLToPath(dir)
+                for (const filePath of await glob(`${path}/**/*.html`)) {
+                    const html = await readFile(filePath, 'utf-8')
+                    const minified = minify(html, {
+                        removeComments: true,
+                        removeAttributeQuotes: true,
+                        collapseWhitespace: true,
+                        minifyJS: true,
+                        minifyCSS: true,
+                    })
+                    await writeFile(filePath, minified)
+                }
+            },
         },
     }
 }
