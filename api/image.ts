bc0c3319a0deea2d1bef7a8a3cddc8840ca03e95
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-05-06 11:36:18 +0800

    fix: function esm error try `allowJs`

--- api/image.ts
+++ api/image.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
 import glob from 'fast-glob'
 
-export default async function image(req: VercelRequest, res: VercelResponse) {
+export default async function (req: VercelRequest, res: VercelResponse) {
     const files = await glob(['**/*', '!node_modules'])
     res.json({
         params: req.query,
