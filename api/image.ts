6c3eef26a38f548ba8bf9c1f923c49234728fcb6
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-05-06 12:00:06 +0800

    Revert "test: vercel server files"
    This reverts commit a32f6fc220d529349103f00d8d82a100a8bd6e73.

--- api/image.ts
+++
@@ -1,8 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node'
-
-export default async function (req: VercelRequest, res: VercelResponse) {
-    res.json({
-        params: req.query,
-        msg: 'holy shit!',
-    })
-}
