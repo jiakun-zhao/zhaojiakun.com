3d0df1ab28d6ab8080ce7b0168e239e6b818404a
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-05-06 11:38:12 +0800

    test: vercel bug fix

--- api/image.ts
+++ api/image.ts
@@ -1,10 +1,8 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import glob from 'fast-glob'
 
 export default async function (req: VercelRequest, res: VercelResponse) {
-    const files = await glob(['**/*', '!node_modules'])
     res.json({
         params: req.query,
-        files,
+        msg: 'holy shit!',
     })
 }
