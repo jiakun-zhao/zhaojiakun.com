2666e2c9b2b5e5babf0bf685d654bbe01e44d46f
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-05-19 23:37:31 +0800

    chore: update fileNesting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,19 +8,9 @@ export default defineNuxtConfig({
     buildAssetsDir: 'assets',
     head: {
       title: 'Jiakun Zhao',
-      htmlAttrs: {
-        lang: 'en',
-      },
-      meta: [
-        { 'http-equiv': 'x-ua-compatible', 'content': 'IE=edge' },
-      ],
-      link: [
-        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
-        { rel: 'shortcut icon', type: 'image/x-icon', href: '/favicon.ico' },
-      ],
+      htmlAttrs: { lang: 'en' },
+      meta: [{ 'http-equiv': 'x-ua-compatible', 'content': 'IE=edge' }],
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }],
     },
   },
 })
