2524abc55e1665221892a60a0a61ab71ade19388
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-05-21 21:14:11 +0800

    chore: change index info

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
 import { useDefaultHead } from '~/utils'
 import IndexWrapper from '~/components/IndexWrapper.vue'
 
-useDefaultHead({ title: 'Jiakun Zhao', description: 'Hi~' })
+useDefaultHead({ title: 'Hi~', description: 'Hi~' })
 
 const links = {
   '/posts': '博客',
@@ -14,7 +14,7 @@ const links = {
 <template>
   <IndexWrapper title="Hi~">
     <p max-w-lg class="[&>span:last-of-type]:hidden">
-      <span>我正在思索如何设计与构建一个足够让我满意的个人网站；在此之前这些以最简单的方式呈现： </span>
+      <span>我正在思索如何设计与构建一个足够让我满意的个人网站，那么在此之前让这些以最简单的方式呈现： </span>
       <template v-for="link of Object.entries(links)" :key="link[1]">
         <RouterLink :to="link[0]">{{ link[1] }}</RouterLink>
         <span>, </span>
