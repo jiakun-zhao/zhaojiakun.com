c76ab4d1c6a84598f4e57498a05cc7138b5cb807
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-05-22 12:38:59 +0800

    feat: @vercel/analytics

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { createApp } from 'vue'
 import { RouterView, createRouter, createWebHistory } from 'vue-router'
 import { createHead } from '@vueuse/head'
+import { inject } from '@vercel/analytics'
 import NProgress from 'nprogress'
-import routes from '~pages'
 import { vShiki } from '~/utils'
-import 'nprogress/nprogress.css'
+import routes from '~pages'
 
 import 'uno.css'
+import 'nprogress/nprogress.css'
 import '~/assets/index.css'
+
+inject()
 
 const router = createRouter({ routes, history: createWebHistory() })
 NProgress.configure({ showSpinner: false })
