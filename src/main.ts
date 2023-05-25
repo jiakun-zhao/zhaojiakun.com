72001931ebc9855e844ec9b1df4fbf4d68eb3985
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-05-25 15:00:40 +0800

    chore: analytics function

--- src/main.ts
+++ src/main.ts
@@ -11,8 +11,14 @@ import '~/assets/index.css'
 const rootLoad = document.querySelector('#root_load') as HTMLDivElement
 
 const router = createRouter({ routes, history: createWebHistory() })
-router.beforeEach(async (to, from, next) => {
+router.beforeEach((to, from, next) => {
   rootLoad.style.display = 'block'
+  if (!import.meta.env.DEV) {
+    fetch('https://fun.zhaojiakun.com/api/personal-website/', {
+      method: 'HEAD',
+      headers: { 'X-To-Path': to.fullPath, 'X-From-Path': from.fullPath },
+    }).then().catch()
+  }
   next()
 })
 router.afterEach(() => rootLoad.style.display = 'none')
