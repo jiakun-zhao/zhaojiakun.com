799d5002efcd0ce092c3823ee5abc23ecfaccc2b
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-05-26 11:53:18 +0800

    remove: cloudflareinsights

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import { RouterView, createRouter, createWebHistory } from 'vue-router'
 import { createHead } from '@vueuse/head'
-import { useScriptTag } from '@vueuse/core'
 import { vImageFigure, vShiki } from '~/utils'
 import routes from '~pages'
 import NeteaseMusicCard from '~/components/NeteaseMusicCard.vue'
@@ -16,14 +15,8 @@ router.beforeEach((to, from, next) => {
   root_load.style.display = 'block'
   next()
 })
-
 router.afterEach(() => {
   root_load.style.display = 'none'
-  useScriptTag(
-    'https://static.cloudflareinsights.com/beacon.min.js',
-    () => {},
-    { defer: true, attrs: { 'data-cf-beacon': '{"token": "813025cf87024fee9e795e698ba9c76a"}' } },
-  )
 })
 
 createApp(RouterView)
